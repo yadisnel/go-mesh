9973ed96233ba5cc80ab12b93012307c4858caf8
Yadisnel Galvez Velázquez <yadisnel@gmail.com>
2021-01-19 17:34:36 -0500

    Update registry.go

--- api/router/registry/registry.go
+++ api/router/registry/registry.go
@@ -10,13 +10,13 @@ import (
 	"sync"
 	"time"
 
-	"github.com/yadisnel/go-ms-cli/v2/api"
-	"github.com/yadisnel/go-ms-cli/v2/api/router"
-	"github.com/yadisnel/go-ms-cli/v2/api/router/util"
-	"github.com/yadisnel/go-ms-cli/v2/logger"
-	"github.com/yadisnel/go-ms-cli/v2/metadata"
-	"github.com/yadisnel/go-ms-cli/v2/registry"
-	"github.com/yadisnel/go-ms-cli/v2/registry/cache"
+	"github.com/yadisnel/go-ms/v2/api"
+	"github.com/yadisnel/go-ms/v2/api/router"
+	"github.com/yadisnel/go-ms/v2/api/router/util"
+	"github.com/yadisnel/go-ms/v2/logger"
+	"github.com/yadisnel/go-ms/v2/metadata"
+	"github.com/yadisnel/go-ms/v2/registry"
+	"github.com/yadisnel/go-ms/v2/registry/cache"
 )
 
 // endpoint struct, that holds compiled pcre
